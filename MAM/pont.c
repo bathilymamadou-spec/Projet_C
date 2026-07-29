b9fb187912d80e008ca773286c9f498df1168481
OulimataBadji <badji.oulimata@uam.edu.sn>
2026-07-29 00:02:40 +0000

    suite

--- MAM/pont.c
+++ MAM/pont.c
@@ -3,7 +3,23 @@
 #include <pont.h>
 
 
-
+void detecter_anomalies_charge(Capteur capteurs[], int n, Alerte alertes[], int *nb_alr) {
+    for (int i = 0; i < n; i++) {
+        if (strcmp(capteurs[i].type, "CHARGE") == 0) {
+            float div = capteurs[i].valeur_mesuree / capteurs[i].valeur_nominale;
+            if (div >= 0.80) {
+                capteurs[i].etat = 2;
+                strcpy(capteurs[i].remarque, "Alerte JAUNE: Utilisation de plus de 80% de la capacité");
+                alertes[(*nb_alr)++] = {"27/07/2026 10:00", i, "SURCHARGE", "JAUNE", , capteurs[i].valeur_mesuree, capteurs[i].valeur_nominale * 0.80, "Diminuer les charges lourdes"};
+            }
+            else if (ratio >= 0.90) {
+                capteurs[i].etat = 3;
+                strcpy(capteurs[i].remarque, "Alerte ROUGE: Utilisation de plus de 90% de la capacité)");
+                Alertes alertes[(*nb_alr)++] = {"27/07/2026 10:00", i, "SURCHARGE", "ROUGE", capteurs[i].valeur_mesuree, capteurs[i].valeur_nominale * 0.90, "Fermeture circulation Poids-Lourds"};
+            }
+        }
+    }
+}
 
 void trier_capteurs_par_type(Capteur capteurs[], int n){
     for (int i = 0; i < n-1; i++){
