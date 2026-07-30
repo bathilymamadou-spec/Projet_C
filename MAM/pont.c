b48610d043783b2378094852cb37c98679c5ca70
OulimataBadji <badji.oulimata@uam.edu.sn>
2026-07-30 20:24:04 +0000

    fin

--- MAM/pont.c
+++ MAM/pont.c
@@ -1,1 +1,70 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "pont.h"
+
+
+
+
+void detecter_anomalies_charge(Capteur capteurs[], int n, Alerte alertes[], int *nb_alr) {
+    for (int i = 0; i < n; i++) {
+        if (strcmp(capteurs[i].type, "CHARGE") == 0) {
+            float div = capteurs[i].valeur_mesuree / capteurs[i].valeur_nominale;
+            if (div >= 0.80) {
+                capteurs[i].etat = 2;
+                strcpy(capteurs[i].remarque, "Alerte JAUNE: Utilisation de plus de 80% de la capacité");
+                Alerte alerts = {"27/07/2026 10:00", i, "SURCHARGE", "JAUNE", capteurs[i].valeur_mesuree, capteurs[i].valeur_nominale * 0.80, "Diminuer les charges lourdes"};
+                alertes[(*nb_alr)++]=alerts;
+            }
+            else if (div >= 0.90) {
+                capteurs[i].etat = 3;
+                strcpy(capteurs[i].remarque, "Alerte ROUGE: Utilisation de plus de 90% de la capacité)");
+                Alerte alerts = {"27/07/2026 10:00", i, "SURCHARGE", "ROUGE", capteurs[i].valeur_mesuree, capteurs[i].valeur_nominale * 0.90, "Fermeture circulation Poids-Lourds"};
+                alertes[(*nb_alr)++]=alerts;
+            }
+        }
+    }
+}
+
+void trier_capteurs_par_type(Capteur capteurs[], int n){
+    for (int i = 0; i < n-1; i++){
+        for (int j = i+1; j < n; j++){
+            if (strcmp(capteurs[i].type, capteurs[j].type) > 0){
+                Capteur copie = capteurs[i];
+                capteurs[i] = capteurs[j];
+                capteurs[j] = copie;
+            }
+        }
+    }
+}
+
+Capteur* recherche_capteur_critique(Capteur capteurs[], int n){
+    Capteur *pointeur = NULL;
+    int max=1;
+    for (int i = 0; i < n; i++){
+        if (capteurs[i].etat > max){
+            max = capteurs[i].etat;
+            pointeur = &capteurs[i];
+        }
+    }
+    return pointeur;
+}
+
+void afficher_menu(){
+    int choix = 0;
+    printf("\n=== SUIVI STRUCTURALE PONT FAIDHERBE ===\n");
+    printf("1. Charger données de mesure\n");
+    printf("2. Valider tous les capteurs\n");
+    printf("3. Afficher état détaillé des capteurs\n");
+    printf("4. Analyser anomalies\n");
+    printf("5. Calculer indice de santé structurale\n");
+    printf("6. Générer rapport d'inspection\n");
+    printf("7. Exporter rapport réglementaire (Eurocode)\n");
+    printf("8. Afficher alertes actives\n");
+    printf("9. Quitter\n");
+    printf("Entrer votre choix\n");
+    scanf("%d", &choix);
+}
+
+
